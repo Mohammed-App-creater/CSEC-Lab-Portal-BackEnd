bc4bcecd78ab916981e9ad79907c35d678acc1c7
Mohammed Ismail <mahammedismail1996@gmail.com>
2025-04-30 21:59:01 +0300

    fix the cors problem

--- src/core/config/cors.config.ts
+++ src/core/config/cors.config.ts
@@ -18,6 +18,6 @@ export const corsOptions: CorsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true
 };
