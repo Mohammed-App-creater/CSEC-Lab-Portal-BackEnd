c12aeb45912a71358f245253ff39dffff23eda30
Mohammed Ismail <mahammedismail1996@gmail.com>
2025-05-02 16:56:30 +0300

    feat: Enhance GetAllEventController to handle pagination with default values for limit and page

--- src/modules/_event/controllers/get-all-events.controller.ts
+++ src/modules/_event/controllers/get-all-events.controller.ts
@@ -4,7 +4,8 @@ import { Request, Response, NextFunction } from "express";
 export const GetAllEventController = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { limit, page } = req.query;
-        const events = await GetAllEventUseCase(Number(limit), Number(page));
+
+        const events = await GetAllEventUseCase(limit? Number(limit): 10, page?  Number(page): 1);
         res.status(200).json(events);
     } catch (error) {
         next(error);
